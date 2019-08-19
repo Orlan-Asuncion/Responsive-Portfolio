5a9c4f593f4e0baceea0c7c43492b2ee45555f70
Orlan-Asuncion <48038688+Orlan-Asuncion@users.noreply.github.com>
2019-08-19 11:10:58 -0400

    Updated portfolio

--- main.js/main.js
+++ main.js/main.js
@@ -1,33 +1,44 @@
 // console.log('It works');
 $(document).ready(function () {
     $('.submit').click(function (event) {
-        event.default()
-        console.log('Clicked button')
+        event.default();
+        console.log('Clicked button');
 
-        var email = $('.email').val()
-        var subject = $('.subject').val()
-        var message = $('.message').val()
-        var statusElm = $('.status')
-        statusElm = empty
+        var email = $('.email').val();
+        var subject = $('.subject').val();
+        var message = $('.message').val();
+        var statusElm = $('.status');
+        statusElm = empty;
 
         if (email.length >= 5 && email.includes('@') && email.includes('.')) {
            
-            statusElm.append('<div>Email is valid</div>')
-        } else { event.default()
-            statusElm.append('<div>Email is not valid</div>')
+            statusElm.append('<div>Email is valid</div>');
+        } else { event.default();
+            statusElm.append('<div>Email is not valid</div>');
         }
 
         if (subject.length >= 2) {
            
-            statusElm.append('<div>subject is valid</div>')
-        } else { event.default()
-            statusElm.append('<div>subject is not valid</div>')
+            statusElm.append('<div>subject is valid</div>');
+        } else { event.default();
+            statusElm.append('<div>subject is not valid</div>');
         }
         if (subject.length >= 20) {
             
-            statusElm.append('<div>message is valid</div>')
-        } else {event.default()
-            statusElm.append('<div>message is not valid</div>')
+            statusElm.append('<div>message is valid</div>');
+        } else {event.default();
+            statusElm.append('<div>message is not valid</div>');
         }
-        })
+        });
+        // $("img").on("click",function(){
+        //      $(this).animate({
+        //          opacity:0.25,g
+        //          left:"+=50",
+        //          height:"toggle"
+        //      }, 5000,function(){
+        //         // animation complete
+        //          console.log("completed!!");
+        //      });
+        //      $(this).fadeToggle("slow","linear");
+        // });
 });
